6963e6a0c699135c696e09fe004719c3b1b2867e
Ikhsan S <93766033+ikhsan3adi@users.noreply.github.com>
2023-05-09 20:03:04 +0700

    chore: init calculator bloc

---
+++ libraries/core/lib/src/bloc/bloc/calculator_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'calculator_event.dart';
+part 'calculator_state.dart';
+
+class CalculatorBloc extends Bloc<CalculatorEvent, CalculatorState> {
+  CalculatorBloc() : super(CalculatorInitial()) {
+    on<CalculatorEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
